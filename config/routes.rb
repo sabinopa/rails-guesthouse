516fb797372d6d1c28139a7c37f818d1e1637609
sabinopa <priasabino@gmail.com>
2023-10-31 09:24:04 -0300

    build: instala gem devise e cria models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :guest, controllers: { 
+    sessions: 'guest/sessions', 
+    registrations: 'guest/registrations'
+  }
+  devise_for :host, controllers: { 
+    sessions: 'host/sessions' 
+  }
+  
   root to: "home#index"
 end

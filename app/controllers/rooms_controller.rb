bc892bd125dfb8d425ae6a668776c663a0e0e491
sabinopa <priasabino@gmail.com>
2023-11-06 19:00:02 -0300

    fix: verifica se guesthouse pertence ao host

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,19 +1,17 @@
 class RoomsController < ApplicationController
   before_action :authenticate_host!
   before_action :set_room, only: [:show, :edit, :update]
-  before_action :set_room_and_check_host, only: [:new, :create, :edit, :update]
+  before_action :set_guesthouse_and_check_host, only: [:new, :create, :edit, :update]
 
   def show
     @guesthouse = current_host.guesthouse
   end
 
   def new
-    @guesthouse = current_host.guesthouse
     @room = Room.new
   end
 
   def create
-    @guesthouse = current_host.guesthouse
     @room = Room.new(room_params)
     @room.guesthouse = @guesthouse
 
@@ -42,8 +40,9 @@ class RoomsController < ApplicationController
                                 :balcony, :air_conditioner, :tv)
   end
 
-  def set_room_and_check_host
-    if Room.find(params[:id]).guesthouse.host != @guesthouse.host
+  def set_guesthouse_and_check_host
+    @guesthouse = Guesthouse.find(params[:guesthouse_id])
+    if @guesthouse.host != current_host
       return redirect_to root_path, notice: 'Ops, você não é o anfitrião dessa pousada.'
     end
   end

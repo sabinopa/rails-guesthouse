84a2247f415b49aafbb75d895d402ce899683967
sabinopa <priasabino@gmail.com>
2023-12-01 07:58:32 -0300

    spec: complementa testes

--- spec/system/visitor/visitor_sees_rooms_details_spec.rb
+++ spec/system/visitor/visitor_sees_rooms_details_spec.rb
@@ -52,4 +52,26 @@ describe 'Visitor sees rooms details' do
 
     expect(current_path).to eq root_path
   end
+
+  it 'and dont see inactive rooms' do
+    host = Host.create!(name: 'Aline', lastname: 'Santos', email: 'aline@email.com', password: 'password')
+    payment_method = PaymentMethod.create!(method: 'Crédito 3x')
+    guesthouse = Guesthouse.create!(host: host, description: 'Atmosfera acolhedora e serviços personalizados', brand_name: 'Pousada Serenidade', 
+                      corporate_name: 'Serenidade Hospedagens Ltda', registration_number: '10.290.988/0001-20', phone_number: '42 98989-0000',
+                      email: 'contato@pousadaencanto.com', address: 'Estrada das Colinas, Km 5', neighborhood: 'Vale Tranquilo', 
+                      city: 'Maceió', state:'AL', postal_code: '12345-67', payment_method_id: payment_method.id, pet_friendly: 'Aceita animais de estimação', 
+                      usage_policy: 'Manter silêncio nas áreas comuns.', checkin: '14:00', checkout: '10:00', status: 1)
+    room1 = Room.create!(guesthouse: guesthouse, name: 'Elegância', description: 'Um quarto sofisticado para uma estadia luxuosa.', size: 25, 
+                      max_people: '2', price: 350.00, air_conditioner: false, bathroom: true, balcony: true, tv: true, wardrobe: true, safe: true, 
+                      accessibility: false, status: :inactive)
+    room2 = Room.create!(guesthouse: guesthouse, name: 'Tranquilidade', description: 'Um ambiente calmo e reconfortante.', size: 15, max_people: '4', 
+                      price: '220,00', bathroom: 'Privado', balcony: 'Não possui', tv: 'Possui', wardrobe: 'Possui', safe: 'Possui', 
+                      accessibility: 'Acessível para pessoas com deficiência', status: :active)
+
+    visit root_path
+    click_on 'Pousada Serenidade - Maceió'
+
+    expect(page).to have_content 'Tranquilidade - 4 pessoas'
+    expect(page).not_to have_content 'Elegância - 2 pessoas'
+  end
 end
--- spec/system/visitor/visitor_views_homepage_spec.rb
+++ spec/system/visitor/visitor_views_homepage_spec.rb
@@ -27,7 +27,7 @@ describe 'Visitor views homepage' do
 
     visit root_path
 
-    expect(page). not_to have_content 'Não existem pousadas cadastrados.'
+    expect(page).not_to have_content 'Não existem pousadas cadastrados.'
     expect(page).to have_content 'Pousada Serenidade - Maceió'
     expect(page).to have_content 'Pousada Maré Alta - Florianópolis'
     expect(page).to have_content 'Pousadas recentes'
@@ -53,7 +53,7 @@ describe 'Visitor views homepage' do
 
     visit root_path
 
-    expect(page). not_to have_content 'Não existem pousadas cadastradas.' 
+    expect(page).not_to have_content 'Não existem pousadas cadastradas.' 
     expect(page).to have_content 'Buscar destinos' 
     expect(page).to have_content 'Maceió' 
     expect(page).to have_content 'Florianópolis' 
@@ -63,5 +63,61 @@ describe 'Visitor views homepage' do
     visit root_path
 
     expect(page).to have_content 'Não existem pousadas cadastradas.' 
+  end
+
+  it 'and see actives guesthouses' do
+    host = Host.create!(name: 'Aline', lastname: 'Santos', email: 'aline@email.com', password: 'password')
+    second_host = Host.create!(name: 'Bruna', lastname: 'Almeida', email: 'bruna@email.com', password: '12345678')
+    payment_method = PaymentMethod.create!(method: 'Crédito 3x')
+    Guesthouse.create!(host: host, description: 'Atmosfera acolhedora e serviços personalizados', brand_name: 'Pousada Serenidade', 
+                      corporate_name: 'Serenidade Hospedagens Ltda', registration_number: '10.290.988/0001-20', phone_number: '42 98989-0000',
+                      email: 'contato@pousadaencanto.com', address: 'Estrada das Colinas, Km 5', neighborhood: 'Vale Tranquilo', 
+                      city: 'Maceió', state:'AL', postal_code: '12345-67', payment_method_id: payment_method.id, pet_friendly: 'Aceita animais de estimação', 
+                      usage_policy: 'Manter silêncio nas áreas comuns.', checkin: '14:00', checkout: '10:00', status: :active)
+
+    Guesthouse.create!(host: second_host, description: 'Um refúgio à beira-mar com vista panorâmica', brand_name: 'Pousada Maré Alta',
+                      corporate_name: 'Maré Alta Hospedagens Ltda', registration_number: '11.111.111/0001-11', phone_number: '55 55555-5555',
+                      email: 'contato@pousadamarealta.com', address: 'Avenida Beira Mar, 123', neighborhood: 'Costa Brilhante',
+                      city: 'Florianópolis', state: 'SC', postal_code: '54321-90', payment_method_id: payment_method.id, 
+                      pet_friendly: 'Aceita animais de estimação', usage_policy: 'Proibido fumar nas dependências.',
+                      checkin: '15:00', checkout: '11:00', status: :active)
+
+    visit root_path
+
+    expect(page).not_to have_content 'Não existem pousadas cadastradas.' 
+    expect(page).to have_content 'Pousadas recentes' 
+    expect(page).to have_content 'Pousada Serenidade - Maceió' 
+    expect(page).to have_content 'Pousada Maré Alta - Florianópolis' 
+  end
+
+  it 'and dont see inactives guesthouses' do
+    host = Host.create!(name: 'Aline', lastname: 'Santos', email: 'aline@email.com', password: 'password')
+    second_host = Host.create!(name: 'Bruna', lastname: 'Almeida', email: 'bruna@email.com', password: '12345678')
+    payment_method = PaymentMethod.create!(method: 'Crédito 3x')
+    Guesthouse.create!(host: host, description: 'Atmosfera acolhedora e serviços personalizados', brand_name: 'Pousada Serenidade', 
+                      corporate_name: 'Serenidade Hospedagens Ltda', registration_number: '10.290.988/0001-20', phone_number: '42 98989-0000',
+                      email: 'contato@pousadaencanto.com', address: 'Estrada das Colinas, Km 5', neighborhood: 'Vale Tranquilo', 
+                      city: 'Maceió', state:'AL', postal_code: '12345-67', payment_method_id: payment_method.id, pet_friendly: 'Aceita animais de estimação', 
+                      usage_policy: 'Manter silêncio nas áreas comuns.', checkin: '14:00', checkout: '10:00', status: :active)
+
+    Guesthouse.create!(host: second_host, description: 'Um refúgio à beira-mar com vista panorâmica', brand_name: 'Pousada Maré Alta',
+                      corporate_name: 'Maré Alta Hospedagens Ltda', registration_number: '11.111.111/0001-11', phone_number: '55 55555-5555',
+                      email: 'contato@pousadamarealta.com', address: 'Avenida Beira Mar, 123', neighborhood: 'Costa Brilhante',
+                      city: 'Florianópolis', state: 'SC', postal_code: '54321-90', payment_method_id: payment_method.id, 
+                      pet_friendly: 'Aceita animais de estimação', usage_policy: 'Proibido fumar nas dependências.',
+                      checkin: '15:00', checkout: '11:00', status: :inactive)
+
+    visit root_path
+
+    expect(page).not_to have_content 'Não existem pousadas cadastradas.' 
+    expect(page).to have_content 'Pousadas recentes' 
+    expect(page).to have_content 'Pousada Serenidade - Maceió' 
+    expect(page).not_to have_content 'Pousada Maré Alta - Florianópolis' 
+  end
+
+  it 'and see search field' do 
+    visit root_path 
+    expect(page).to have_button 'Buscar Pousada'  
+    expect(page).to have_link 'Busca detalhada'
   end
 end
